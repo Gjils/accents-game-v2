38be04c7b80f4d85006a00b03d8fa5adee224cd4
Alexey <preobrazhenskyalexsey@gmail.com>
2024-06-09 21:21:49 +0300

    fix

--- frontend/src/App/view/App.tsx
+++ frontend/src/App/view/App.tsx
@@ -3,6 +3,7 @@ import { useEffect } from "react";
 export default function App() {
   useEffect(() => {
     fetch("http://backend:5000/test").then((data) => console.log(data));
+    fetch("http://backend:5000/edffsdfgsdfg").then((data) => console.log(data));
   }, []);
   return <div>App</div>;
 }
