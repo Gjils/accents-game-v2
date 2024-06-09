02a9a13d80f2af6af0ce52bae5f74ad412735d0b
Alexey <preobrazhenskyalexsey@gmail.com>
2024-06-10 01:18:32 +0300

    fix

--- frontend/src/App/view/App.tsx
+++ frontend/src/App/view/App.tsx
@@ -2,9 +2,7 @@ import { useEffect } from "react";
 
 export default function App() {
   useEffect(() => {
-    fetch(`${import.meta.env.VITE_SERVER_URL}/api/test`).then((data) =>
-      console.log(data),
-    );
+    fetch(`/api/test`).then((data) => console.log(data));
   }, []);
   return <div>App</div>;
 }
