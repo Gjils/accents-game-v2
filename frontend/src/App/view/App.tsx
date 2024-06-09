9f8c6596955f17bce86a9b4161cfa83e1f02ab13
Alexey <preobrazhenskyalexsey@gmail.com>
2024-06-10 01:01:12 +0300

    fix

--- frontend/src/App/view/App.tsx
+++ frontend/src/App/view/App.tsx
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 
 export default function App() {
   useEffect(() => {
-    fetch(`http://${import.meta.env.SERVER_URL}/api/test`).then((data) =>
+    fetch(`http://${import.meta.env.VITE_SERVER_URL}/api/test`).then((data) =>
       console.log(data),
     );
   }, []);
---
+++ frontend/vite.config.js
@@ -0,0 +1,15 @@
+import path from "path";
+
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react-swc";
+
+// https://vitejs.dev/config/
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+  return defineConfig({
+    plugins: [react()],
+    resolve: {
+      alias: { "@/": `${path.resolve(__dirname, "./src")}/` },
+    },
+  });
+};
