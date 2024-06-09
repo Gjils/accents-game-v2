27865685d5d6dd52a96758619c5867645ccfeea8
Alexey <preobrazhenskyalexsey@gmail.com>
2024-06-09 21:23:12 +0300

    fix

--- frontend/src/App/view/App.tsx
+++ frontend/src/App/view/App.tsx
@@ -4,6 +4,7 @@ export default function App() {
   useEffect(() => {
     fetch("http://backend:5000/test").then((data) => console.log(data));
     fetch("http://backend:5000/edffsdfgsdfg").then((data) => console.log(data));
+    fetch("http://89.169.54.5:5000/test").then((data) => console.log(data));
   }, []);
   return <div>App</div>;
 }
