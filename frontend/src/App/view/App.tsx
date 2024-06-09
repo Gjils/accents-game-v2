dbeb25733dd8ff26626a66e5bf0519c6596fbe82
Alexey <preobrazhenskyalexsey@gmail.com>
2024-06-10 00:04:56 +0300

    add nginx proxy

--- frontend/src/App/view/App.tsx
+++ frontend/src/App/view/App.tsx
@@ -2,9 +2,7 @@ import { useEffect } from "react";
 
 export default function App() {
   useEffect(() => {
-    fetch("http://backend:5000/test").then((data) => console.log(data));
-    fetch("http://backend:5000/edffsdfgsdfg").then((data) => console.log(data));
-    fetch("http://89.169.54.5:5000/test").then((data) => console.log(data));
+    fetch("http://89.169.54.5:80/api/test").then((data) => console.log(data));
   }, []);
   return <div>App</div>;
 }
