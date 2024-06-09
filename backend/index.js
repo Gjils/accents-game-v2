598f2bec274350674d8761d6145280027c631e08
Alexey <preobrazhenskyalexsey@gmail.com>
2024-06-09 20:49:52 +0300

    add cors

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,20 @@
-import express from "express"
+import express from "express";
 
-const app = express()
-const port = 3000
+import cors from "cors";
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const app = express();
+const port = 5000;
+
+let corsOptions = {
+	origin: ["http://frontend:3000"],
+};
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+	res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+	console.log(`Example app listening on port ${port}`);
+});
