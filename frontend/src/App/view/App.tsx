9f32073102b17bee75787413b82ba86ee0d02764
Alexey <preobrazhenskyalexsey@gmail.com>
2024-06-09 20:31:32 +0300

    fix

--- frontend/src/App/view/App.tsx
+++ frontend/src/App/view/App.tsx
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 
 export default function App() {
   useEffect(() => {
-    fetch("http://backend:3000/").then((data) => console.log(data));
+    fetch("http://backend:5000/").then((data) => console.log(data));
   }, []);
   return <div>App</div>;
 }
